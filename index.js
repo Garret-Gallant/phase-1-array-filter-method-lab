a82ed3f198121ce5259b13cd179acb6002e08c1a
Garret Gallant <gallanttechnological@gmail.com>
2022-07-04 23:27:43 -0600

    commit tree efbda87d3616670e4d688924b294a30e3ca30a16

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+function findMatching(foo){
+    console.log(foo(drivers))
+}
+
+const matchDrivers = (array) => {for (item of array){if(item === String){return(item)}}}
+
+findMatching(matchDrivers);
+
+// let games = [
+//     {
+//         title: `Dishonored`,
+//         genre: `Stealth`,
+//         rating: `10/10`
+//     },
+//     {
+//         title: `Rocket League`,
+//         genre: `Sports`,
+//         rating: `8/10` 
+//     },
+//     {
+//         title: `Smite`,
+//         genre: `MOBA`,
+//         rating: `6/10` 
+//     },
+//     {
+//         title: `Stardew Valley`,
+//         genre: `Farming`,
+//         rating: `10/10` 
+//     },
+//     {
+//         title: `Apex Legends`,
+//         genre: `FPS`,
+//         rating: `9/10` 
+//     }
+// ]
+
+// function gameReview(foo){
+//     console.log(foo(games))
+// }
+
+// function returnTitles(array){
+//     let total = 0
+//     for (let item of array){
+//         total+= item.title
+//         console.log(item.title)
+//     }
+// }
+
+// const matchStardew = (array) => {for (item of array){if(item.genre === `Farming`){console.log(item)}}}
+
+// gameReview(returnTitles)
+
+// gameReview(matchStardew)
+
